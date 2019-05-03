0216bef84a4aed2c0d9c9b4a934d695e979f5e2b
unknown <naultmichael1@gmail.com>
2019-05-03 18:03:02 -0400

    Update HomePage

--- Final/assets/javascript/logic.js
+++ Final/assets/javascript/logic.js
@@ -1,4 +1,18 @@
+$(document).ready(function () {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $("#myModal").modal();
+    $('#demo-carousel-auto').carousel();
+    setInterval(function () {
+        $('#demo-carousel-auto').carousel('next');
+    }, 1000);
+
+});
+
+
 // Initialize Firebase
+
 var config = {
     apiKey: "AIzaSyBHLLHSID8XnKBEARSlZ2xsp9kHrNXjRgo",
     authDomain: "onedayoff-2291e.firebaseapp.com",
