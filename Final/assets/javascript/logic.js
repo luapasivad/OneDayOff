0743d61f63f486b6ed1be643e8e905d113ce4196
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-05-03 18:34:43 -0400

    added home-responsiveness

--- Final/assets/javascript/logic.js
+++ Final/assets/javascript/logic.js
@@ -1,14 +1,3 @@
-$(document).ready(function () {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal').modal();
-    $('.sidenav').sidenav();
-    $("#myModal").modal();
-    $('#demo-carousel-auto').carousel();
-    setInterval(function () {
-        $('#demo-carousel-auto').carousel('next');
-    }, 1000);
-
-});
 
 
 // Initialize Firebase
